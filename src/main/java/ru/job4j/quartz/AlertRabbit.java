7c5ebb7d37c9b07229fdf32817073d90250e6434
kostas-111 <galkink370@gmail.com>
2024-10-27 22:52:48 +0300

    1.1. Job c параметрами [#260360].Доработка программы AlertRabbit: добавил в файл rabbit.properties настройки БД, создается коннект и делается запись в таблицу, когда выполнена Job

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,8 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -12,7 +14,7 @@ import static org.quartz.TriggerBuilder.*;
 
 public class AlertRabbit {
 
-    /**
+    /*
      * Метод чтения файла с настройками, который будет возвращать загруженный Properties
      */
     private static Properties init() {
@@ -26,61 +28,72 @@ public class AlertRabbit {
         }
     }
 
-    public static void main(String[] args) {
+    /*
+     * scheduler - Создание класса, управляющего всеми работами.
+     * В объект Scheduler мы будем добавлять задачи, которые хотим выполнять периодически
+     * job - Создание задачи. Quartz каждый раз создает объект с типом org.quartz.Job.
+     * Класс, реализующий этот интерфейс - Rabbit
+     * times - Создание расписания. Настраивает периодичность запуска. В нашем случае запуск задачи происходит
+     * через определенное количество секунд. Период обновления хранится в файле rabbit.properties
+     * по ключу rabbit.interval. repeatForever() - означает что делаем это бесконечно.
+     * trigger - Задача выполняется через триггер. Здесь можно указать, когда начинать запуск.
+     * Мы хотим сделать это сразу - startNow()
+     * scheduler.scheduleJob(job, trigger) - Загрузка задачи и триггера в планировщик.
+     * Весь main работает 10 секунд
+     */
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName(init().getProperty("jdbc.driver-class-name"));
         try {
-            /*
-             * Создание класса, управляющего всеми работами.
-             * В объект Scheduler мы будем добавлять задачи, которые хотим выполнять периодически
-             */
+            Connection con = DriverManager.getConnection(
+                   init().getProperty("jdbc.url"),
+                   init().getProperty("jdbc.username"),
+                   init().getProperty("jdbc.password")
+            );
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-
-            /*
-             * Создание задачи.
-             * quartz каждый раз создает объект с типом org.quartz.Job.
-             * Класс, реализующий этот интерфейс - Rabbit
-             */
-            JobDetail job = newJob(Rabbit.class).build();
-
-            /*
-             * Создание расписания. Настраивает периодичность запуска.
-             * В нашем случае запуск задачи происходит через определенное количество секунд.
-             * Период обновления хранится в файле rabbit.properties по ключу rabbit.interval.
-             * repeatForever() - означает что делаем это бесконечною
-             */
+            JobDataMap data = new JobDataMap();
+            data.put("connection", con);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(Integer.parseInt(
                             init().getProperty("rabbit.interval"))
                     )
                     .repeatForever();
-
-            /*
-             * Задача выполняется через триггер.
-             * Здесь можно указать, когда начинать запуск.
-             * Мы хотим сделать это сразу - startNow()
-             */
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
-
-            /*
-             * Загрузка задачи и триггера в планировщик.
-             */
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            con.close();
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
-    /**
+    /*
      * Внутри этого класса нужно описать требуемые действия.
-     * В нашем случае - это вывод на консоль текста
      */
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO sql_schema.rabbit(created_date) values (?)")) {
+                preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
