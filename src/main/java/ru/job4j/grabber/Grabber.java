3f3ef89335a642acca00ce63e7ab7515e9a834d5
kostas-111 <galkink370@gmail.com>
2024-11-17 19:23:31 +0300

    7. Web. [#289478 #522311] Добавил возможность получить данные через браузер от приложения

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,6 +5,10 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 import static org.quartz.JobBuilder.newJob;
@@ -58,22 +62,58 @@ public class Grabber implements Grab {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                posts.forEach(store :: save);
+                posts.forEach(store::save);
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        var config = new Properties();
+    /*
+    Сервер. Ответ от сервера будет в виде списка вакансий.
+    С помощью этого метода имеем возможность получить данные через браузер
+    от нашего граббера
+     */
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg().getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    /*
+    метод для получения объекта типа Properties,
+    чтобы получать значения из конфиг-файла в используемых методах
+     */
+    private static Properties cfg() throws IOException {
         try (InputStream input = Grabber.class.getClassLoader()
                 .getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
             config.load(input);
+            return config;
         }
+    }
+
+    public static void main(String[] args) throws Exception {
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
         scheduler.start();
         var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
         var store = new PsqlStore();
-        var time = Integer.parseInt(config.getProperty("time"));
-        new Grabber(parse, store, scheduler, time).init();
+        var time = Integer.parseInt(cfg().getProperty("time"));
+        Grabber grab = new Grabber(parse, store, scheduler, time);
+        grab.init();
+        grab.web(store);
     }
 }
